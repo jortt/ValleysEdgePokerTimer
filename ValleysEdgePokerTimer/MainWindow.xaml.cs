5f84ffb8129721d5c154d54f07da0645b7064496
Jacob Ortt <jacobortt@gmail.com>
2016-06-19 22:35:42 -0600

    Added images and cleaned up colour scheme

--- ValleysEdgePokerTimer/MainWindow.xaml.cs
+++ ValleysEdgePokerTimer/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace ValleysEdgePokerTimer
         {
             InitializeComponent();
         }
+
+        private void PlayBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PlayBtn.Source = new BitmapImage(new Uri("/res/Play2.png", UriKind.Relative));
+        }
+
+        private void PlayBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PlayBtn.Source = new BitmapImage(new Uri("/res/Play1.png", UriKind.Relative));
+        }
+
+        private void StopBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+           StopBtn.Source = new BitmapImage(new Uri("/res/Stop2.png", UriKind.Relative));
+        }
+
+        private void StopBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StopBtn.Source = new BitmapImage(new Uri("/res/Stop1.png", UriKind.Relative));
+        }
+
+        private void RewindBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            RewindBtn.Source = new BitmapImage(new Uri("/res/Rwd2.png", UriKind.Relative));
+        }
+
+        private void RewindBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            RewindBtn.Source = new BitmapImage(new Uri("/res/Rwd1.png", UriKind.Relative));
+        }
+
+        private void FastForwardBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            FastForwardBtn.Source = new BitmapImage(new Uri("/res/Fwd2.png", UriKind.Relative));
+        }
+
+        private void FastForwardBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            FastForwardBtn.Source = new BitmapImage(new Uri("/res/Fwd1.png", UriKind.Relative));
+        }
     }
 }
